3de50cbb488905f96e66d550404044251b426ac5
Phong Trinh <thanhphongus@gmail.com>
2022-03-09 09:43:17 -0700

    setup validationResult to send back error to user

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -13,6 +13,11 @@ router.post('/api/users/signup', [
     .withMessage('Password must be between 4 and 20 characters')
 
 ], (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array());
+  }
   const { email, password } = req.body;
 
   if (!email || typeof email != 'string') {
