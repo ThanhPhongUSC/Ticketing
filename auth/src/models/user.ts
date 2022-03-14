d5a40de25ee32e0b965a3d553057a47c9851b1e8
Phong Trinh <thanhphongus@gmail.com>
2022-03-14 14:13:32 -0600

    add custom method into model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -6,6 +6,12 @@ interface UserAttrs {
   password: string;
 }
 
+// An interface that describes the properties that a User Model has
+
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -17,11 +23,18 @@ const userSchema = new mongoose.Schema({
   }
 })
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs)
+}
 
-const buildUser = (attrs: UserAttrs) => {
-  return new User(attrs);
-};
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
+// const buildUser = (attrs: UserAttrs) => {
+//   return new User(attrs);
+// };
+User.build({
+  email: 'test@test.com',
+  password: 'password'
+})
 
-export { User, buildUser };
+export { User };
