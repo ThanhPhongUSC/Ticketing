c9b89ae0a1bcb03e11267fa35a7e37dff2718a58
Phong Trinh <thanhphongus@gmail.com>
2022-03-19 10:09:42 -0600

    validation email and pw in signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
+router.post('/api/users/signin', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid')
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('You must supply a password')
+
+](req, res) => {
   res.send('Hi there!')
 });
 
