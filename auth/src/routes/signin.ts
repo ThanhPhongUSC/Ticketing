42fb0d2ae207644915d63c6608664b4b83106fe3
Phong Trinh <thanhphongus@gmail.com>
2022-03-21 13:54:05 -0600

    checking if the login pw match the pw store in db

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,6 +4,7 @@ import { body } from 'express-validator';
 import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 
 const router = express.Router();
@@ -23,6 +24,11 @@ router.post('/api/users/signin', [
 
     const exitingUser = await User.findOne({ email, password });
     if (!exitingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(exitingUser.password, password);
+    if (!passwordsMatch) {
       throw new BadRequestError('Invalid credentials');
     }
   });
