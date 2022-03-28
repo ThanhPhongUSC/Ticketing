ac92dcd14bc393eb5e94cf56c8d6f2a608d1dafd
Phong Trinh <thanhphongus@gmail.com>
2022-03-28 09:20:50 -0600

    create require auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,9 +4,8 @@ import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
-}
 });
 
 export { router as currentUserRouter };
