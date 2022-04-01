d2d6edc970ffe83455a15a634e364eddbcf53253
Phong Trinh <thanhphongus@gmail.com>
2022-04-01 08:37:31 -0600

    test supplied incorrect email in signin route

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('fail when an email that does not exist is supplied', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(400)
+});
