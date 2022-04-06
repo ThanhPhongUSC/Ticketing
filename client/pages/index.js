5b6525648b71dee6b136b434a1e91eb27083d432
Phong Trinh <thanhphongus@gmail.com>
2022-04-06 12:21:37 -0600

    edit getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
@@ -8,8 +8,9 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
 
-  const { data } = await buildClient(context).get('/api/users/currentuser');
+  const { data } = await client.get('/api/users/currentuser');
 
   return data;
 };
