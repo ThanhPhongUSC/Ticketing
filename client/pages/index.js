2f1b348d797faa9fa1299610ef0b9ec095d8aa24
Phong Trinh <thanhphongus@gmail.com>
2022-04-06 12:17:49 -0600

    move axios out of getInitialprops and create a reuseable api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   // axios.get('/api/users/currentuser')
@@ -6,23 +7,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing page</h1>
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
+LandingPage.getInitialProps = async (context) => {
 
-  if (typeof window === 'undefined') {
-    //we are on the server!
-    // request should be made to http://ingress-nginx-controller.ingress-nginx...
-    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: req.headers
-    });
-    return data;
-  } else {
-    //we are on the browser
-    //request can be made with a base url ''
-    const { data } = await axios.get('/api/users/currentuser');
+  const { data } = await buildClient(context).get('/api/users/currentuser');
 
-    return data;
-  }
-  return {};
-}
+  return data;
+};
 
 export default LandingPage;
