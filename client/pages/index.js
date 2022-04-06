98775116204b07a0216461edad92814449759481
Phong Trinh <thanhphongus@gmail.com>
2022-04-06 11:56:52 -0600

    passin g cookie to the request from the SSR in getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,14 +6,13 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing page</h1>
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
+
   if (typeof window === 'undefined') {
     //we are on the server!
     // request should be made to http://ingress-nginx-controller.ingress-nginx...
     const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: {
-        Host: 'ticketing.dev'
-      }
+      headers: req.headers
     });
     return data;
   } else {
