f3b9ac12ce790d8a40650a6cef3c89f04d3714cf
Phong Trinh <thanhphongus@gmail.com>
2022-04-06 14:31:58 -0600

    consitionally showing links in header

--- client/components/header.js
+++ client/components/header.js
@@ -1,5 +1,19 @@
 import Link from 'next/link';
 export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' }
+  ]
+    .filter(linkConfig => linkConfig)
+    .map(({ label, href }) => {
+      return <li key={href} className="nav-item">
+        <Link href={href}>
+          <a className="nav-link">{label}</a>
+        </Link>
+      </li>
+    });
+
   return (<nav className="navbar navbar-light bg-light">
     <Link href="/">
       <a className="navbar-brand">TicketHub</a>
@@ -7,7 +21,7 @@ export default ({ currentUser }) => {
 
     <div className="d-flex justify-content-end">
       <ul className="nav d-flex align-items-center">
-        {currentUser ? 'Sign out' : 'Sign in/up'}
+        {links}
       </ul>
     </div>
   </nav>
