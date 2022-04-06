592659f507a4e216fe27d080f0a2717c91e13df9
Phong Trinh <thanhphongus@gmail.com>
2022-04-06 10:40:31 -0600

    setting up request from browser in getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,23 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  // axios.get('/api/users/currentuser')
+  console.log(currentUser);
   return <h1>Landing page</h1>
+};
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    //we are on the server!
+    // request should be made to http://ingress-nginx-controller.ingress-nginx...
+  } else {
+    //we are on the browser
+    //request can be made with a base url ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
 }
+
+export default LandingPage;
