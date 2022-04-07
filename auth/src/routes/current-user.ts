ab8c33392b2ba6671b1e8cf0592d75817ec9a325
Phong Trinh <thanhphongus@gmail.com>
2022-04-07 13:45:32 -0600

    update import statement in auth service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@phongtickets/common';
 
 
 const router = express.Router();
