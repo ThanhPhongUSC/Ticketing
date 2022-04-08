e3536845040b594499872f3cbcf0fcc695dacc5e
Phong Trinh <thanhphongus@gmail.com>
2022-04-08 12:23:38 -0600

    setting up test for new route

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post request', async () => {
+
+})
+it('can only be accessed if the user is signed in', async () => {
+
+})
+it('return an error if an invalid title is provided', async () => {
+
+})
+it('return an error if an invalid price is provided', async () => {
+
+})
+it('create a ticket with valid inputs', async () => {
+
+})
