91dd7c244dfee535202389a8adc271d7645923ec
Phong Trinh <thanhphongus@gmail.com>
2022-04-09 11:16:12 -0600

    test if the user is signed in return status other than 401

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -14,8 +14,17 @@ it('can only be accessed if the user is signed in', async () => {
     .post('/api/tickets')
     .send({})
 
-  expect(response.statusCode).not.toEqual(401);
+  expect(response.status).toEqual(401);
 })
+
+it('returns a status other than 401 if the user is signed in', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .send({})
+
+  expect(response.status).not.toEqual(401);
+})
+
 it('return an error if an invalid title is provided', async () => {
 
 })
