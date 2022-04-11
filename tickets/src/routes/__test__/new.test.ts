a3f06d984c08aff8311b6c3352e69f9a2e97a131
Phong Trinh <thanhphongus@gmail.com>
2022-04-11 14:27:45 -0600

    create test for new ticket with valid input

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -64,5 +64,13 @@ it('return an error if an invalid price is provided', async () => {
     .expect(400);
 })
 it('create a ticket with valid inputs', async () => {
+  // add in a check to make sure a ticket was saved
 
+  await request(app)
+    .post('/api/tickets')
+    .send({
+      title: 'asdfh',
+      price: 20
+    })
+    .expect(201)
 })
