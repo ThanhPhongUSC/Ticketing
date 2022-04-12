31408890d8454693ec7b6d2ae34c62218f60443d
Phong Trinh <thanhphongus@gmail.com>
2022-04-12 15:29:57 -0600

    setting up ticket updating test

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 404 if the provide id does not exist', async () => {
+
+})
+it('returns a 401 if the user is not authenticated', async () => {
+
+})
+it('returns a 401 if the user does not won the ticket', async () => {
+
+})
+it('returns a 400 if the user provide an invalid title or price', async () => {
+
+})
+it('updates the ticket provide valid inputs', async () => {
+
+})
