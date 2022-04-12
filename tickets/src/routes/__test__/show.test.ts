b16222bcd78aa8dbe68a9371285748876c87e4a6
Phong Trinh <thanhphongus@gmail.com>
2022-04-12 10:12:56 -0600

    setting up show ticket test

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 404 if the ticket is not found', async () => {
+
+})
+
+it('return a ticket if the ticket is found', async () => {
+
+})
