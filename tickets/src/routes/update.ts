791c8333e8b3719e03abb9b9945020211f86442f
Phong Trinh <thanhphongus@gmail.com>
2022-04-13 10:10:17 -0600

    implement validate title and price update for a ticket

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,7 +5,15 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+router.put('/api/tickets/:id', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be provide and must be greater than 0')
+], validateRequest, async (req: Request, res: Response) => {
   const ticket = await Ticket.findById(req.params.id);
 
   if (!ticket) {
