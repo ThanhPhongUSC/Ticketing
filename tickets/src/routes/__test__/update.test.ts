1360f2e05aec32e07035a8015d3529fb42afcde5
Phong Trinh <thanhphongus@gmail.com>
2022-04-13 09:49:02 -0600

    test if the user does not own the ticket in updating ticket

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -23,8 +23,23 @@ it('returns a 401 if the user is not authenticated', async () => {
     })
     .expect(401)
 })
-it('returns a 401 if the user does not won the ticket', async () => {
+it('returns a 401 if the user does not own the ticket', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asdfgh',
+      price: 20
+    });
 
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asdfghjkll'
+  price: 1000
+    })
+    .expect(401);
 })
 it('returns a 400 if the user provide an invalid title or price', async () => {
 
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -35,7 +35,7 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload. {id, email}
   const payload = {
-    id: '1lk24hghgh',
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com'
   }
 
