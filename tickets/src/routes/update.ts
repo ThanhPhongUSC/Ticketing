75eac5c43ace8ffcf30c99edc4b79afa96ca599e
Phong Trinh <thanhphongus@gmail.com>
2022-04-13 09:20:28 -0600

    setting up updating ticket route

---
+++ tickets/src/routes/update.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest, NotFoundError, requireAuth, NotAuthorizedError } from '@phongtickets/common';
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.put('/api/tickets/:id', async (req: Request, res: Response) => {
+
+})
+
+export { router as updateTicketRouter };
