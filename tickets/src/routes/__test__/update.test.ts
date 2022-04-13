19610d041e5dae69cfebce66c34b10389cc47eb1
Phong Trinh <thanhphongus@gmail.com>
2022-04-13 08:38:31 -0600

    test if the provide id does not exist in update route

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,8 +1,17 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 404 if the provide id does not exist', async () => {
-
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asdfgh',
+      price: 20
+    })
+    .expect(404)
 })
 it('returns a 401 if the user is not authenticated', async () => {
 
