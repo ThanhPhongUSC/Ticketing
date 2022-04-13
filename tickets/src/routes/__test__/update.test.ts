3571ed7dd304beb3011cd376d63e833a4393a17c
Phong Trinh <thanhphongus@gmail.com>
2022-04-13 10:22:18 -0600

    test update ticket when the valid inputs is provided

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -42,8 +42,8 @@ it('returns a 401 if the user does not own the ticket', async () => {
     .expect(401);
 })
 it('returns a 400 if the user provide an invalid title or price', async () => {
-
   const cookie = global.signin();
+
   const response = await request(app)
     .post('/api/tickets')
     .set('Cookie', cookie)
@@ -71,5 +71,30 @@ it('returns a 400 if the user provide an invalid title or price', async () => {
     .expect(400);
 })
 it('updates the ticket provide valid inputs', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'asdfgh',
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual('new title');
+  expect(ticketResponse.body.price).toEqual(100);
 
 })
